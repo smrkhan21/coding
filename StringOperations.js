7ffc4e0346aac22c4c7e4faeef6f54473cbc9f0e
Sameer Khan <sameerpathan1997@gmail.com>
2023-09-09 01:57:58 +0530

    Day 5

---
+++ StringOperations.js
@@ -0,0 +1,27 @@
+// 12. Implement a function to perform string multiplication (e.g., "2" * "3" -> "6").
+
+function stringMultiplication(a, b) {
+    return a * b;
+}
+
+function stringAddition(a, b, c) {
+    return a + b + c;
+}
+
+function stringSubstraction(a, b, c) {
+    return a - b - c;
+}
+
+function stringDivision(a, b) {
+    return a / b;
+}
+
+function stringOperation(a, b, c, d, e) {
+    return a * b + c - d / e;
+}
+
+console.log(stringMultiplication("2", "3"));
+console.log(stringAddition("2", "3", "4"));
+console.log(stringSubstraction("2", "3", "4"));
+console.log(stringDivision("2", "3"));
+console.log(stringOperation("2", "3", "4", "5", "6"));
