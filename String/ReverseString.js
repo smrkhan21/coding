830e02e3a032ec0b6d738b5069502d8581a26e64
Sameer Khan <sameerpathan1997@gmail.com>
2023-09-09 23:47:08 +0530

    Updated main

--- String/ReverseString.js
+++
@@ -1,9 +0,0 @@
-function revString(input) {
-    let str = input
-    let revString = ""
-    for(var i = str.length - 1; i>=0; i--) {
-        revString += str[i]
-    }
-    return revString;
-}
-console.log(revString("Sameer"));
