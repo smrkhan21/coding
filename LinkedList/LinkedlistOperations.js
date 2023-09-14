1ada40c8a2b7040efca234dfc49b5a216b5da54e
Sameer Khan <sameerpathan1997@gmail.com>
2023-09-15 01:10:37 +0530

    Day 11

---
+++ LinkedList/LinkedlistOperations.js
@@ -0,0 +1,71 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Insertion at the end
+    append(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Deletion by value
+    delete(data) {
+        if (!this.head) {
+            return;
+        }
+
+        if (this.head.data === data) {
+            this.head = this.head.next;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            if (current.next.data === data) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    // Traversal
+    display() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage:
+const myList = new LinkedList();
+myList.append(1);
+myList.append(2);
+myList.append(3);
+myList.append(4);
+
+console.log("Original Linked List:");
+myList.display();
+
+myList.delete(2);
+console.log("\nLinked List after deleting 2:");
+myList.display();
