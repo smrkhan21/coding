350f82421199735772bb7d4bca25b83e4a0ce6ac
Sameer Khan <sameerpathan1997@gmail.com>
2023-09-16 03:01:26 +0530

    Day 13

--- JsMethodsImplementation/every.js
+++ JsMethodsImplementation/every.js
@@ -11,19 +11,20 @@
  let result = numbers.every(isEven)
  ouput: false
 */
-function customEvery(inputArray, callback) {
-    for(let i = 0; i<inputArray.length; i++) {
-        if(!callback(inputArray[i], i, inputArray)) {
+
+Array.prototype.customEvery = function(callback) {
+    for(let i = 0; i<this.length; i++) { // 'this' here is array(numbers)
+        if(!callback(this[i], i, this)) {
             return false;
         }
     }
     return true;
-}
-
-Array.prototype.customEvery = customEvery;
+};
 
 const numbers = [1, 2, 3, 4, 5];
-
+function isEven(num) {
+    return num % 2 === 0
+}
 // Using the custom every() function
-const isAllEven = numbers.customEvery((num) => num % 2 === 0);
+const isAllEven = numbers.customEvery(isEven);
 console.log(isAllEven); // false, not all numbers are even
